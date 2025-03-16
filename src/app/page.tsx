4d583c002439967ec89874963ec57609fe4918b7
supreme2580 <victoromorogbe69@gmail.com>
2025-03-16 10:35:33 +0100

    ui bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="w-full h-screen bg-black flex flex-col items-center justify-center">
       <div>
-        <Tabs defaultValue="account" className="w-xl space-y-3.5">
+        <Tabs defaultValue="deposit" className="w-xl space-y-3.5">
           <TabsList className="h-fit p-1.5 rounded-full bg-transparent border-2 border-[#1E1E1E] space-x-1.5">
             <TabsTrigger value="deposit" className="hover:cursor-pointer text-base">Deposit</TabsTrigger>
             <TabsTrigger value="withdraw" className="hover:cursor-pointer text-base">Withdraw</TabsTrigger>
